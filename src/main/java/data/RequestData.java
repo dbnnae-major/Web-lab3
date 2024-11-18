43e9318d06f910b7fbfe3e909ac2e8f2002687ec
dbnnae-major <artemd321@gmail.com>
2024-11-18 19:09:45 +0300

    некоторые изменения

--- src/main/java/data/RequestData.java
+++ src/main/java/data/RequestData.java
@@ -2,42 +2,59 @@ package data;
 
 import org.json.JSONObject;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name = "requestData", eager = true)
+@SessionScoped
 public class RequestData {
-    public RequestData(double x, double y, double r, boolean flag) {
-        this.x = x;
-        this.y = y;
-        this.r = r;
-        this.flag = flag;
-    }
     private double x;
     private double y;
     private double r;
     private boolean flag;
 
+    public RequestData() {
+    }
 
     public double getX() {
         return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
     }
 
     public double getY() {
         return y;
     }
 
+    public void setY(double y) {
+        this.y = y;
+    }
+
     public double getR() {
         return r;
     }
 
-    public boolean getFlag() {
+    public void setR(double r) {
+        this.r = r;
+    }
+
+    public boolean isFlag() {
         return flag;
     }
 
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
+
     @Override
-    public String toString(){
+    public String toString() {
         JSONObject jsonData = new JSONObject();
-        jsonData.put("x",x);
-        jsonData.put("y",y);
-        jsonData.put("r",r);
-        jsonData.put("flag",flag);
+        jsonData.put("x", x);
+        jsonData.put("y", y);
+        jsonData.put("r", r);
+        jsonData.put("flag", flag);
         return jsonData.toString();
     }
 }
---
+++ src/main/java/services/AreaCheckBean.java
@@ -0,0 +1,48 @@
+package services;
+
+import data.RequestData;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean(name = "areaCheck", eager = true)
+@SessionScoped
+public class AreaCheckBean {
+    private String result;
+
+    public AreaCheckBean() {}
+
+    public void checkArea() {
+        RequestData requestData = (RequestData) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("requestData");
+
+        double x = requestData.getX();
+        double y = requestData.getY();
+        double r = requestData.getR();
+        boolean flag = areaConfirm(x, y, r);
+
+        if (flag) {
+            result = "POPAL";
+        } else {
+            result = "NE POPAL";
+        }
+        System.out.println(result);
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public boolean areaConfirm(Double x, Double y, Double r) {
+        if ((x >= -r && x <= 0) && (y >= 0 && y <= (double) r / 2)) {
+            return true;
+        }
+        if ((x >= 0 && x <= r) && (y <= ((double) -x / 2 + (double) r / 2) && y >= 0)) {
+            return true;
+        }
+        if ((x * x + y * y) <= (double) (r * r) / 2 && y <= 0 && x <= 0) {
+            return true;
+        }
+        return false;
+    }
+}
